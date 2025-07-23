1a60ba43ff73a301be9f9cb103296c19e5d79d9d
sheikhmaazraheel <sheikhmaazraheel@gmail.com>
2025-07-23 07:29:33 -0700

    secure:true

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: false, // Set to true in production with HTTPS
+      secure: true, // Set to true in production with HTTPS
       maxAge: 1000 * 60 * 60,
     },
   })
