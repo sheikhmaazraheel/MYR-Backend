923883ac5d6917bc33dfbc0d3ba12540c3520331
sheikhmaazraheel <sheikhmaazraheel@gmail.com>
2026-01-14 23:24:09 +0500

    Fix Module changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { sendOrderEmail } from "./utils/sendOrderEmail.js";
-
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const multer = require("multer");
